eb09887bdd14e04a5e2bab56ac2e7d85ae52a81b
Sam Gabbard <slgabbar@ucsc.edu>
2020-01-22 21:44:04 -0800

    account registration

---
+++ mysite/account/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from account.models import Account
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=60, help_text="Required. Add a valid email address.")
+
+    class Meta:
+        model = Account
+        fields = ("username", "email", "password1", "password2")
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from layout.views import home_view
+from account.views import registration_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
+    path('register/', registration_view, name='register'),
 ]
