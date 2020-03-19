bdc6435f2c0065e3d21c43cd3e2e646084d2fe57
Sam Gabbard <samgabbard@Sams-MacBook-Pro.local>
2020-03-18 20:33:55 -0700

    more css

--- mysite/player/models.py
+++ mysite/player/models.py
@@ -6,8 +6,10 @@ User = user_model()
 
 class Player(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player', null=True)
+    # player_id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    # numer = models.CharField()
 
     def __str__(self):
         return self.first_name + " " + self.last_name
