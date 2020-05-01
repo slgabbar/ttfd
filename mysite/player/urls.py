34681acb5eaaa78b36bf80b5f6a83fdb94cc05e9
Sam Gabbard <samgabbard@Sams-MacBook-Pro.local>
2020-05-01 13:01:47 -0700

    switched player model to CBV's

--- mysite/player/urls.py
+++ mysite/player/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_player/', views.add_player, name='add_player'),
-    path('edit_player/<int:pk>', views.edit_player, name='edit_player'),
-    path('delete_player/<int:pk>', views.delete_player, name='delete_player')
+    path('detail/<int:pk>', views.PlayerDetail.as_view(), name='detail_player'),
+    path('add_player/', views.PlayerCreate.as_view(), name='add_player'),
+    path('edit_player/<int:pk>', views.PlayerUpdate.as_view(), name='edit_player'),
+    path('delete_player/<int:pk>', views.PlayerDelete.as_view(), name='delete_player'),
 ]
