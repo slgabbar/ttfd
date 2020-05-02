4cd82b3beeb86578192950e2bc72c6a7adb9d044
Sam Gabbard <samgabbard@Sams-MacBook-Pro.local>
2020-05-01 20:26:02 -0700

    updated views for required login states

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
@@ -6,5 +7,7 @@ urlpatterns = [
     path('register/', views.registration_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
-    path('edit_team/<int:pk>', views.TeamUpdate.as_view(), name='edit_team'),
+    # path('edit_team/<int:pk>', views.TeamUpdate.as_view(), name='edit_team'),
+    path('edit_team/<int:pk>', login_required(views.TeamUpdate.as_view()), name='edit_team'),
+
 ]
--- mysite/player/urls.py
+++ mysite/player/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+
 
 from . import views
 
 urlpatterns = [
-    path('detail/<int:pk>', views.PlayerDetail.as_view(), name='detail_player'),
-    path('add_player/', views.PlayerCreate.as_view(), name='add_player'),
-    path('edit_player/<int:pk>', views.PlayerUpdate.as_view(), name='edit_player'),
-    path('delete_player/<int:pk>', views.PlayerDelete.as_view(), name='delete_player'),
+    path('detail/<int:pk>', login_required(views.PlayerDetail.as_view()), name='detail_player'),
+    path('add_player/', login_required(views.PlayerCreate.as_view()), name='add_player'),
+    path('edit_player/<int:pk>', login_required(views.PlayerUpdate.as_view()), name='edit_player'),
+    path('delete_player/<int:pk>', login_required(views.PlayerDelete.as_view()), name='delete_player'),
 ]
