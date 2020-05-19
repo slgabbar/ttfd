b7a09d5188c45ae4fd8a5ad1cbf2d766074d8fbd
Sam Gabbard <samgabbard@Sams-MacBook-Pro.local>
2020-05-19 16:36:12 -0700

    more styling for gamme scout

--- mysite/game/static/game/js/players.js
+++ mysite/game/static/game/js/players.js
@@ -1,28 +1,37 @@
 function player_clicked(e) {
 	var player = d3.select(e);
 
-	if (player_selected) {
-		// if some player has already been selected
-		if (player.classed("player-clicked")) {
-			//if selected player is clicked player, unclick player
-			// player.classed("player-clicked", false);
-			unclick_player();
+	if (GAME_READY) {
+		if (PLAYER_SELECTED) {
+			// if some player has already been selected
+			if (player.classed("player-clicked")) {
+				//if selected player is clicked player, unclick player
+				player.classed("player-clicked", false);
+				player.classed("player-button", true);
+				unclick_player();
+			} else {
+				// unclick whoever was selected, and select current player
+				unclick_player(); 
+				player.classed("player-clicked", true);
+				player.classed("player-button", false);
+				PLAYER_SELECTED = true;
+				enable_stats();
+			}
 		} else {
-			// unclick whoever was selected, and select current player
-			unclick_player(); 
 			player.classed("player-clicked", true);
-			player_selected = true;
+			player.classed("player-button", false);
+			PLAYER_SELECTED = true;
+			enable_stats();
 		}
-	} else {
-		player.classed("player-clicked", true);
-		player_selected = true;
 	}
 }
 
 function unclick_player() {
 	var player = d3.select(".player-clicked");
 	player.classed("player-clicked", false);
-	player_selected = false;
+	player.classed("player-button", true);
+	PLAYER_SELECTED = false;
+	disable_stats();
 }
 
 function onDragStart(e) {
@@ -61,7 +70,37 @@ function checkGameReady() {
 	var check = d3.selectAll(".empty");
 	if (check.empty()) {
 		GAME_READY = true;
+		start_game();
 	}
 }
+
+function enable_stats() {
+	d3.selectAll(".stat-button").each(function(d) {
+		var button = d3.select(this);
+		button.classed("disabled", false);
+	});
+}
+
+function disable_stats() {
+	d3.selectAll(".stat-button").each(function(d) {
+		var button = d3.select(this);
+		button.classed("disabled", true);
+	});
+}
+
+function start_game() {
+	d3.select(".play-by-play").style("border", "solid black 3px");
+
+	d3.selectAll(".player-button").each(function(d) {
+		var player = d3.select(this);
+		player.classed("in-game", true);
+		player.classed("text-muted", false);
+	})
+
+}
+
+
+
+
 
 
--- mysite/game/static/game/js/scout_game.js
+++ mysite/game/static/game/js/scout_game.js
@@ -1,42 +1,42 @@
 var stat_box = d3.select(".stat-button-container")
 var record_table = d3.select(".play-by-play").select(".table").select("tbody");
 
-function add_button_row(btn1, type1, btn2, type2) {
-	row = stat_box.append("div").attr("class", "button-row");
-	left_button = row.append("div").attr("class", "left-btn");
-	right_button = row.append("div").attr("class", "right-btn")
+// function add_button_row(btn1, type1, btn2, type2) {
+// 	row = stat_box.append("div").attr("class", "button-row");
+// 	left_button = row.append("div").attr("class", "left-btn");
+// 	right_button = row.append("div").attr("class", "right-btn")
 
-	left_button.append("button")
-			// .attr("class", type1)
-			.attr("class", type1 + " btn-block")
-			.text(btn1);
+// 	left_button.append("button")
+// 			// .attr("class", type1)
+// 			.attr("class", type1 + " btn-block")
+// 			.text(btn1);
 
-	right_button.append("button")
-			// .attr("class", type2)
-			.attr("class", type2 + " btn-block")
-			.text(btn2);
+// 	right_button.append("button")
+// 			// .attr("class", type2)
+// 			.attr("class", type2 + " btn-block")
+// 			.text(btn2);
 
-	left_button.on("click", function() {
-		if (player_selected) {
-			player = d3.select(".player-clicked")
+// 	left_button.on("click", function() {
+// 		if (player_selected) {
+// 			player = d3.select(".player-clicked")
 
-			record_stat(player.text(), btn1)
-			player.classed("player-clicked", false);
-			player_selected = false;
-		}
-	});
+// 			record_stat(player.text(), btn1)
+// 			player.classed("player-clicked", false);
+// 			player_selected = false;
+// 		}
+// 	});
 
-	right_button.on("click", function() {
-		if (player_selected) {
-			player = d3.select(".player-clicked")
-			record_stat(player.text(), btn2);
-			player.classed("player-clicked", false);
-			player_selected = false;
-		}
-	});
-}
+// 	right_button.on("click", function() {
+// 		if (player_selected) {
+// 			player = d3.select(".player-clicked")
+// 			record_stat(player.text(), btn2);
+// 			player.classed("player-clicked", false);
+// 			player_selected = false;
+// 		}
+// 	});
+// }
 
-function record_stat(player, stat) {
-	// console.log(player, stat);
-	var table = record_table.append("tr");
-}
+// function record_stat(player, stat) {
+// 	// console.log(player, stat);
+// 	var table = record_table.append("tr");
+// }
